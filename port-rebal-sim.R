3f26019f0bc0ca98cb0c90cb3d7192d164d74ba0
Aaron Kirkman <kirkman.aaron.w+github@gmail.com>
2020-12-13 14:27:40 -0500

    split each rebalancing model into separate functions

--- port-rebal-sim.R
+++ port-rebal-sim.R
@@ -4,7 +4,8 @@ set.seed(1)
 
 # Parameters --------------------------------------------------------------
 num_periods <- 100
-rebalance_every <- 4
+# rebalance_every <- 4
+# rmse_threshold <- 0.01
 
 # Assets ------------------------------------------------------------------
 shocks <- c(
@@ -15,27 +16,81 @@ shocks <- c(
 # Allocation --------------------------------------------------------------
 target_allocation <- c(0.90, 0.10)
 
-# Simulation --------------------------------------------------------------
-with_reb <- matrix(nrow = num_periods, ncol = length(shocks))
-without_reb <- matrix(nrow = num_periods, ncol = length(shocks))
-
-with_reb[1, ] <- target_allocation
-without_reb[1, ] <- target_allocation
 
 
-for (t in 2:num_periods) {
-  returns <- sapply(shocks, function(f) f())
-  without_reb[t, ] <- without_reb[t-1, ] * (1 + returns)
-  with_reb[t, ] <- with_reb[t-1, ] * (1 + returns)
-  if (t %% rebalance_every == 0) {
-    with_reb[t, ] <- sum(with_reb[t, ]) * target_allocation
+
+# Utility Functions -------------------------------------------------------
+portfolio_return <- function(portfolio_values) {
+  return(sum(portfolio_values[nrow(portfolio_values), ]) / sum(portfolio_values[1, ]) - 1)
+}
+
+rmse <- function(vector1, vector2) {
+  return(sqrt(sum(abs(vector1 - vector2)^2)))
+}
+
+
+
+# Simulation Functions--------------------------------------------------------------
+model_no_rebalancing <- function(target_allocation, num_periods, shocks) {
+  # target_allocation: The ideal allocation for the portfolio. In this model
+  # (with no rebalancing), this is only used to initialize the portfolio
+
+  # num_periods: The number of periods to run the simulation
+
+  # shocks: A vector of functions representing the probability distributions
+  # from which the returns are drawn
+  portfolio <- matrix(nrow = num_periods, ncol = length(target_allocation))
+  portfolio[1, ] <- target_allocation
+  for (t in 2:num_periods) {
+    returns <- sapply(shocks, function(f) f())
+    portfolio[t, ] <- portfolio[t-1, ] * (1 + returns)
   }
+  return(portfolio_return(portfolio))
 }
 
-total_portfolio_return <- function(portfolio_values) {
-  N <- nrow(portfolio_values)
-  return(sum(portfolio_values[N, ]) / sum(portfolio_values[1, ]) - 1)
+model_cyclical_rebalancing <- function(target_allocation, num_periods, rebalance_every, shocks) {
+  # target_allocation: The ideal allocation for the portfolio. Rebalancing brings
+  # the portfolio back to this allocation
+
+  # num_periods: The number of periods to run the simulation
+
+  # rebalance_every: Rebalance every time this many periods have elapsed, AFTER the portfolio
+  # value is calculated using the randomly-drawn returns for that period
+
+  # shocks: A vector of functions representing the probability distributions
+  # from which the returns are drawn
+  portfolio <- matrix(nrow = num_periods, ncol = length(target_allocation))
+  portfolio[1, ] <- target_allocation
+  for (t in 2:num_periods) {
+    returns <- sapply(shocks, function(f) f())
+    portfolio[t, ] <- portfolio[t-1, ] * (1 + returns)
+    if (t %% rebalance_every == 0) {
+      portfolio[t, ] <- sum(portfolio[t, ]) * target_allocation
+    }
+  }
+  return(portfolio_return(portfolio))
 }
 
-sprintf("%3.2f%%", total_portfolio_return(without_reb) * 100)
-sprintf("%3.2f%%", total_portfolio_return(with_reb) * 100)
+model_rmse_rebalancing <- function(target_allocation, num_periods, rmse_threshold, shocks) {
+  portfolio <- matrix(nrow = num_periods, ncol = length(target_allocation))
+  portfolio[1, ] <- target_allocation
+  for (t in 2:num_periods) {
+    returns <- sapply(shocks, function(f) f())
+    portfolio[t, ] <- portfolio[t-1, ] * (1 + returns)
+    if (rmse(target_allocation, portfolio[t, ]) > rmse_threshold) {
+      portfolio[t, ] <- sum(portfolio[t, ]) * target_allocation
+    }
+  }
+  return(portfolio_return(portfolio))
+}
+
+
+# Simulation --------------------------------------------------------------
+num_simulation_runs <- 50
+simulation_results <- matrix(nrow = num_simulation_runs, ncol = 3)
+for (i in 1:num_simulation_runs) {
+  print(sprintf("Simulation #%d", i))
+  simulation_results[i, 1] = model_no_rebalancing(target_allocation, num_periods, shocks)
+  simulation_results[i, 2] = model_cyclical_rebalancing(target_allocation, num_periods, rebalance_every = 4, shocks)
+  simulation_results[i, 3] = model_rmse_rebalancing(target_allocation, num_periods, rmse_threshold = 0.05, shocks)
+}
